1021509fdc384ee4476d41016176f01a8d7bcfc6
Hannah Perry <hannah.perry@utexas.edu>
2017-06-17 17:50:31 -0500

    reverting to master branch cuz of internal server error.

--- application.py
+++ application.py
@@ -1,45 +1,34 @@
 from flask import Flask
 
-# # print a nice greeting.
-# def say_hello():
-#     return '<p>Testing deployment after setting up custom domain!</p>\n'
+# print a nice greeting.
+def say_hello():
+    return '<p>Testing deployment after setting up custom domain 123!</p>\n'
 
-# # some bits of text for the page.
-# header_text = '''
-#     <html>\n<head> <title>StreamGlean</title> </head>\n<body>'''
-# instructions = '''
-#     <p><em>Hint</em>: This is a RESTful web service! Append a username
-#     to the URL (for example: <code>/Thelonious</code>) to say hello to
-#     someone specific.</p>\n'''
-# home_link = '<p><a href="/">Back</a></p>\n'
-# footer_text = '</body>\n</html>'
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>StreamGlean</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
 
-# # EB looks for an 'application' callable by default.
-# application = Flask(__name__)
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
-# # add a rule for the index page.
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello()  + footer_text))
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello()  + footer_text))
 
-# # add a rule when the page is accessed with a name appended to the site
-# # URL.
-# # application.add_url_rule('/<username>', 'hello', (lambda username:
-# #     header_text + say_hello(username) + home_link + footer_text))
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return '<p>Hey there Hannah</p>'
-
-if __name__ == '__main__':
-    app.run()
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+# application.add_url_rule('/<username>', 'hello', (lambda username:
+#     header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
