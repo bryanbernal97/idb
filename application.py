81223625b3a3262afdb7983c037c23106366ba57
Hannah Perry <hannah.perry@utexas.edu>
2017-06-17 13:41:13 -0500

    Downloaded free Start Bootstrap theme, Agency, as a jumping off point for streamglean. Theme found here: https://startbootstrap.com/template-overviews/agency/

--- application.py
+++ application.py
@@ -1,34 +1,45 @@
+from flask import Flask, render_template
+
+# # print a nice greeting.
+# def say_hello():
+#     return '<p>Testing deployment after setting up custom domain!</p>\n'
+
+# # some bits of text for the page.
+# header_text = '''
+#     <html>\n<head> <title>StreamGlean</title> </head>\n<body>'''
+# instructions = '''
+#     <p><em>Hint</em>: This is a RESTful web service! Append a username
+#     to the URL (for example: <code>/Thelonious</code>) to say hello to
+#     someone specific.</p>\n'''
+# home_link = '<p><a href="/">Back</a></p>\n'
+# footer_text = '</body>\n</html>'
+
+# # EB looks for an 'application' callable by default.
+# application = Flask(__name__)
+
+# # add a rule for the index page.
+# application.add_url_rule('/', 'index', (lambda: header_text +
+#     say_hello()  + footer_text))
+
+# # add a rule when the page is accessed with a name appended to the site
+# # URL.
+# # application.add_url_rule('/<username>', 'hello', (lambda username:
+# #     header_text + say_hello(username) + home_link + footer_text))
+
 from flask import Flask
+app = Flask(__name__)
 
-# print a nice greeting.
-def say_hello():
-    return '<p>Testing deployment after setting up custom domain!</p>\n'
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>StreamGlean</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+@app.route('/')
+def hello():
+    return render_template('index.html')
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello()  + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
+if __name__ == '__main__':
+    app.run()
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
