53f718f2212f65c839ac1955d78cf9aea7e570b9
Hannah Perry <hannah.perry@utexas.edu>
2017-07-01 20:33:51 -0500

    Trying to add a config file and do db stuff in same way as tutorial.

--- application.py
+++ application.py
@@ -1,22 +1,13 @@
 from flask import Flask
 from flask import render_template
+from application import db
+from application.models import Data
 
-from models import db
 
 
 # Create the Flask app
 application = Flask(__name__)
 
-POSTGRES = {
-    'user': 'group2',
-    'pw': 'cs373group2',
-    'db': 'streamglean_db',
-    'host': 'streamglean-rds.cxx60yvk87ey.us-east-2.rds.amazonaws.com',
-    'port': '5432',
-}
-application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-
-db.init_app(application)
 
 # print a nice greeting.
 
@@ -73,27 +64,6 @@ def show_gfe():
 def show_cloud9():
     return render_template('cloud9.html')
 
-# # some bits of text for the page.
-# header_text = '''
-#     <html>\n<head> <title>StreamGlean</title> </head>\n<body>'''
-# instructions = '''
-#     <p><em>Hint</em>: This is a RESTful web service! Append a username
-#     to the URL (for example: <code>/Thelonious</code>) to say hello to
-#     someone specific.</p>\n'''
-# home_link = '<p><a href="/">Back</a></p>\n'
-# footer_text = '</body>\n</html>'
-
-# # EB looks for an 'application' callable by default.
-# application = Flask(__name__)
-
-# # add a rule for the index page.
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello()  + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":
