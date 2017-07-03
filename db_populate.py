41ff37fbd4b7b7944ead2388ac7b98ac65cb9612
Hannah Perry <hannah.perry@utexas.edu>
2017-07-03 14:10:36 -0500

    Wrote User model.

---
+++ db_populate.py
@@ -0,0 +1,7 @@
+# This is where we are making the calls to the API's and populating our data
+
+#Ignore for now
+
+# from application import db
+# from application.models import Channel
+# from application.models import Stream
