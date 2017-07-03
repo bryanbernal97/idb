4e60d0cb85925630ff08e8276e8e00188329e35f
Hannah Perry <hannah.perry@utexas.edu>
2017-07-03 16:04:59 -0500

    Fixed create file.

--- db_create.py
+++ db_create.py
@@ -1,8 +1,8 @@
 from application import db
-# from application.models import User
-# from application.models import Game
-# from application.models import Team
-# from application.models import Community
+from application.models import User
+from application.models import Game
+from application.models import Team
+from application.models import Community
 
 
 db.create_all()
