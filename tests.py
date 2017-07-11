2a9ff9f72c57d5c240cb94bb59135db8f5e2e062
Hannah Perry <hannah.perry@utexas.edu>
2017-07-11 18:12:08 -0500

    Added test for updating game with invalid id.

--- tests.py
+++ tests.py
@@ -991,7 +991,32 @@ class TestApi(TestCase):
 
     def test_update_game_invalid(self):
         # Test API PUT method api/game
-        self.assertTrue(True)
+
+        test_id = -1
+        new_test_name = 'NEW API TEST UPDATE GAME INVALID'
+
+        # Make sure db instance does not have instance with test_id
+        try:
+            query = GAME.query.get(test_id)
+            self.assertisNone(query)
+            db.session.close()
+        except:
+            db.session.rollback()
+
+        update = {'name': new_test_name}
+
+        # Update the user through the API
+        response = requests.put(self.game_url + '/' + str(test_id), data=json.dumps(update), headers=self.headers)
+
+        self.assertEqual(response.status_code, 404)
+
+        # Make sure db instance still does not have instance with test_id
+        try:
+            query = Game.query.get(test_id)
+            self.assertisNone(query)
+            db.session.close()
+        except:
+            db.session.rollback()
 
 
     def test_update_team_valid(self):
