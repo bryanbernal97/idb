139c306df6eba333b6804f50d4fd0bab081ce949
Hannah Perry <hannah.perry@utexas.edu>
2017-07-12 14:22:26 -0500

    Updated pagination indicators on users page.

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@ var innerClass = {class:"text-muted"};
 var wallOfText = "Twitch.tv is a website where streamers live stream themselves providing content centered around their lives and video games. There are major facets of this experience that we display in the following sections: User, Game, Community, Team. A streamer(user) plays different games, takes part in different established communities centered around these games, and often join various esports teams which their streams then represent. Included with all of them are many other media and information which enhances each of those models. Twitch contains most of this information but we wanted to bring this information together in an easy to navigate environment.  We have named our website StreamGlean.me (glean - extract (information) from various sources) to represent the process of gathering stream information and sharing it with you guys."
 ReactDOM.render(
 	r('div', outerClass, null,
-		r('small', innerClass, wallOfText)), document.getElementById('root'));
+		r('small', innerClass, wallOfText)), document.getElementById('aboutText'));
 
 // var searchNames = ['Sydney', 'Melbourne', 'Brisbane', 
 //     'Adelaide', 'Perth', 'Hobart'];
